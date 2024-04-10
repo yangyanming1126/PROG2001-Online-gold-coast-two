5a68ae44704cf2a5c0c69addb6c68ea79d3db9ba
2640650447 <y.fu.17@student.scu.edu.au>
2024-04-10 13:00:10 +0800

    Script

---
+++ Assets/Menu/Scripts/NavScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NavScript : MonoBehaviour
+{
+    public void loadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
+
