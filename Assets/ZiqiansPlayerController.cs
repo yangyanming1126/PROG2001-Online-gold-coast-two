a603fd41cdbe1be9bb1fe285720f6f549352bba9
guziqian03 <z.gu.13@student.scu.edu.au>
2024-04-11 19:14:04 +0800

    total

--- Assets/ZiqiansPlayerController.cs
+++ Assets/ZiqiansPlayerController.cs
@@ -38,6 +38,7 @@ public class ZiqiansPlayerController : MonoBehaviour
     {
         if (other.gameObject.CompareTag("PickUp"))
         {
+            GetComponent<AudioSource>().Play();
             other.gameObject.SetActive(false);
             count += 1;
 
